a3c26a5cfcf50e2eeaeae025424d600e8626400f
asadh00 <asadhujjaman00@gmail.com>
2025-01-28 20:01:54 +0000

    allowed all hosts for now , added 2nd admin

--- shopping/settings.py
+++ shopping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#0npbkfydx^+1sh8*_(h=f0&kl6p55msi4f6tt)=)e@g178_eo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shopping-fuio.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
